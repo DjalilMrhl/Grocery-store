cd383d654691b9ac8ad96c83bf72c0cbf4828670
DjalilMrhl <121756355+DjalilMrhl@users.noreply.github.com>
2023-04-30 12:38:45 +0100

    Update ProductsAPI.js

--- src/Redux/API/ProductsAPI.js
+++ src/Redux/API/ProductsAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productsAPI = createApi({
     reducerPath: "products",
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:1337/api'}),
+    baseQuery: fetchBaseQuery({baseUrl: `${process.env.BACKEND_URL}/api`}),
     endpoints: builder => ({
         getAllProducts: builder.query({
             query: () => 'products?populate=image'

e29ba97d90b78b6b2a1f70cc94a8202c8d2758ee
DjalilMrhl <121756355+DjalilMrhl@users.noreply.github.com>
2023-05-01 19:45:46 +0100

    Update ProductsAPI.js

--- src/Redux/API/ProductsAPI.js
+++ src/Redux/API/ProductsAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productsAPI = createApi({
     reducerPath: "products",
-    baseQuery: fetchBaseQuery({baseUrl: `http://localhost:1337/api`}),
+    baseQuery: fetchBaseQuery({baseUrl: `https://grocer-store.netlify.app/api`}),
     endpoints: builder => ({
         getAllProducts: builder.query({
             query: () => 'products?populate=image'

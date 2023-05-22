887d3802428b19dbb9740a45037ec05d42debc33
unknown <marhoul2200@gmail.com>
2023-05-22 17:25:28 +0100

    update

---
+++ src/data.js
@@ -0,0 +1,85 @@
+import avocado from './assets/avocado.jpg'
+import orange from './assets/orange.png'
+import salmon from './assets/salmon.jpg'
+import mollusk from './assets/Mollusk.png'
+import beetroot from './assets/beetroot.jpg'
+import asparagus from './assets/asparagus.jpg'
+import shark from './assets/shark.jpg'
+import octopus from './assets/octopus.png'
+import banana from './assets/banana.png'
+
+
+export const data = [
+    {
+        title: 'Avocado',
+        description: 'The avocado (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.5,
+        previousPrice: 3,
+        category: 'vegetables',
+        thumbnail: avocado
+    },
+    {
+        title: 'Orange',
+        description: 'The avocado (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.79,
+        previousPrice: 3.29,
+        category: 'fruits',
+        thumbnail: orange
+    },
+    {
+        title: 'Beetroot',
+        description: 'The avocado (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 3.59,
+        previousPrice: 5.19,
+        category: 'vegetables',
+        thumbnail: beetroot
+    },
+    {
+        title: 'Salmon',
+        description: 'The avocado (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 4.69,
+        previousPrice: 3.89,
+        category: 'seafood',
+        thumbnail: salmon
+    },
+    {
+        title: 'Asparagus',
+        description: 'The Asparagus (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.79,
+        previousPrice: 3.69,
+        category: 'vegetables',
+        thumbnail: asparagus
+    },
+    {
+        title: 'Banana',
+        description: 'The Banana (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 3.79,
+        previousPrice: 4.69,
+        category: 'fruits',
+        thumbnail: banana
+    },
+    {
+        title: 'Mollusk',
+        description: 'The Mollusk (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.79,
+        previousPrice: 3.69,
+        category: 'seafood',
+        thumbnail: mollusk
+    },
+    {
+        title: 'Octopus',
+        description: 'The Octopus (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.79,
+        previousPrice: 3.69,
+        category: 'seafood',
+        thumbnail: octopus
+    },
+    {
+        title: 'Shark',
+        description: 'The Shark (Persea americana) is a medium-sized, evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated ...',
+        price: 2.79,
+        previousPrice: 3.69,
+        category: 'seafood',
+        thumbnail: shark
+    }
+]

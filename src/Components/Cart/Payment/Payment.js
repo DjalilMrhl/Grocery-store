64e8beb884faac538da051a3477e8ef33bcc9dc5
DjalilMrhl <121756355+DjalilMrhl@users.noreply.github.com>
2023-05-22 18:41:18 +0100

    Update Payment.js

--- src/Components/Cart/Payment/Payment.js
+++ src/Components/Cart/Payment/Payment.js
@@ -3,25 +3,15 @@ import "./Payment.scss";
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { Color, ShippingForm } from "../../../Context/context";
-import axios from 'axios'
 
 function Payment() {
 
-  const cartItems = useSelector(state=> state.cart.cartItems)
-  const {form, setForm} = useContext(ShippingForm)
   const {setActive} = useContext(Color)
   
   const navigate = useNavigate()
   const makePayment = ()=> {
      navigate("/cart/confirmation")
   }
-
-  const handleChange = (e)=> {
-    setForm(prev=> {
-      return {...prev, [e.target.name]: e.target.value}
-    })
-  }
-
   const handleClickBack = ()=> {
     navigate("/cart/shipping")
     setActive(2)
@@ -32,10 +22,10 @@ function Payment() {
       <h3>Contact informations</h3>
       <div>
         <label>Email
-          <input name="email" type="email" onChange={handleChange}/>
+          <input name="email" type="email"/>
         </label>
         <label>Phone
-          <input name="phoneNumber" type="Number" onChange={handleChange}/>
+          <input name="phoneNumber" type="Number"/>
         </label>
       </div>
       <div className="btns">

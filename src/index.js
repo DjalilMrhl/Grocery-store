da8c5527f04f52edfd0d646597caa79f1a2c2bc2
DjalilMrhl <121756355+DjalilMrhl@users.noreply.github.com>
2023-05-22 17:59:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,12 @@ import App from './App';
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import cartReducer from './Redux/Slices/cartSlice.js'
-import { productsAPI } from './Redux/API/ProductsAPI'
-// import authReducer from './Redux/Slices/AuthSlice.js';
 import { Context } from './Context/context'
 
 export const store = configureStore({
   reducer: {
-    [productsAPI.reducerPath] : productsAPI.reducer,
     cart: cartReducer
-    // auth: authReducer
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(productsAPI.middleware)
+  }
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
